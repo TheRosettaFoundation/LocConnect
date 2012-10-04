b9f9ff5f394c139703ee9cfb1c53a9979a0e72d2
Sean Mooney <sean.mooney@ul.ie>
2012-10-04 20:13:38 +0100

    replased lenght with sizeof

--- create_project_xliff.php
+++ create_project_xliff.php
@@ -207,7 +207,7 @@ function sendResource($id, $type, $metdata, $desc, $content)
      /* XML processing goes here */
         $headers = $xliff->getElementsByTagName( 'header' );
         
-        if($headers==null||$headers->lenght==0){// creat a header element if it dose not exist
+        if($headers==null||sizeof($headers)==0){// creat a header element if it dose not exist
             $fileElement = $xliff->getElementsByTagName( 'file' )->item(0);
             $header=$xliff->createElement('header');
             $fileElement->appendChild($header);
